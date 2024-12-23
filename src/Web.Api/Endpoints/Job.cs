52d0238948841c9fda3bf097f24c0d5a5d672b71
Aleksandar Devic <mr.aleksandar.devic@gmail.com>
2024-12-23 01:26:38 +0100

    Bug fixed.

--- src/Web.Api/Endpoints/Job.cs
+++ src/Web.Api/Endpoints/Job.cs
@@ -125,7 +125,7 @@ public class Job : IEndpoint
             [FromBody] CompleteJobItemRequest request,
             CancellationToken cancellationToken) =>
         {
-            var command = new CompleteJobItemCommand(jobInProgressId, jobItemId, request.EnteredQuantity, request.Status);
+            var command = new CompleteJobItemCommand(jobInProgressId, jobItemId, request.RequiredFieldRead1, request.RequiredFieldRead2, request.RequiredFieldRead3, request.Status);
             var result = await sender.Send(command, cancellationToken);
 
             return result.Match(Results.NoContent, CustomResults.Problem);
