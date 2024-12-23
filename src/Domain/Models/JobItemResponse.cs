aa4fe1ad6a6c707240d29c5bdc53246e47568cfe
Aleksandar Devic <mr.aleksandar.devic@gmail.com>
2024-12-23 23:23:27 +0100

    Added short in responses.

--- src/Domain/Models/JobItemResponse.cs
+++ src/Domain/Models/JobItemResponse.cs
@@ -9,6 +9,7 @@ public record JobItemResponse
     public long JobId { get; set; }
 
     public string? ItemDescription { get; set; }
+    public string? ItemDescriptionShort => GetJobItemDescriptionShort(this.ItemDescription);
     public string? RequiredField1 { get; set; }
     public string? RequiredField2 { get; set; }
     public double? RequiredField3 { get; set; }
@@ -17,4 +18,14 @@ public record JobItemResponse
 
     public double? ReadedField3 { get; set; }
     public JobItemStatus ItemStatus { get; set; }
+
+    private static string? GetJobItemDescriptionShort(string? itemDescription)
+    {
+        if (itemDescription is not null && itemDescription.Contains(','))
+        {
+            var commaIndex = itemDescription.IndexOf(',');
+            return itemDescription[(commaIndex + 1)..].Trim();
+        }
+        return itemDescription;
+    }
 }
--- src/Domain/Models/JobResponse.cs
+++ src/Domain/Models/JobResponse.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Domain.Enums;
 
 namespace Domain.Models;
@@ -6,6 +7,7 @@ public class JobResponse
 {
     public required long Id { get; set; }
     public required string? Description { get; set; }
+    public string? DescriptionShort => GetDescriptionShort(this.Description);
     public required short? AssignedOperatorId { get; set; }
     public required JobType Type { get; set; }
     public required DateTime CreationDateTime { get; set; }
@@ -15,4 +17,14 @@ public class JobResponse
     public required int? InventoryId { get; set; }
     public required bool IsVerified { get; set; }
     public required string? Client { get; set; }
+
+    private static string? GetDescriptionShort(string? description)
+    {
+        if (string.IsNullOrWhiteSpace(description))
+            return string.Empty;
+
+        var match = Regex.Match(description, @"^\d+/\d+/\d+/\d+");
+
+        return match.Success ? match.Value : string.Empty;
+    }
 }
